c38435803104c756eae82e9db0cf136ce5ebc816
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-07-11 14:07:43 -0400

    DevDojo JDBC

---
+++ DevDojo/Docker/src/main/java/Aula255/JDBCpt04/InstalandoEAdicionandoMavenAoProjeto/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package Aula255.JDBCpt04.InstalandoEAdicionandoMavenAoProjeto.test;
+
+import Aula255.JDBCpt04.InstalandoEAdicionandoMavenAoProjeto.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
