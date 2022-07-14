cdd5ce8feadb280db07dcda291713dc817b69db7
Ezxykdriv <mm.matheusfalcao@gmai.com>
2022-07-13 22:29:47 -0400

    Tipos Primitivos

---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 10;
+        long numeroGrande = 1000000L;
+        double salarioDouble = 2000.0D;
+        float slarioFloat = 2500.0F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        String nome = "Goku";
+
+        System.out.println("A idade eh " + idade + " anos");
+        System.out.println(falso);
+        System.out.println("char " + caractere);
+        System.out.println("Oi, mue nome é " + nome);
+    }
+}
