2cc700899e8172b536747549146fd5ad013c5615
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-07-19 14:38:50 -0400

    Operadores

--- DevDojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,9 +2,60 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // + - * /
         int numero02 = 20;
         int numero01 = 10;
-        int resultado = numero02+numero01;
+        double resultado = numero01/ (double) numero02;
         System.out.println(resultado);
+
+        //&
+        int resto = 21% 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        boolean isDezDiferenteQueVinte = 10 != 20;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMaiorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezMaiorQueVinte " + isDezIgualQueVinte);
+        System.out.println("isDezMaiorQueVinte " + isDezDiferenteQueVinte);
+        System.out.println();
+
+        //&&(AND) ||(OR) !(NOT)
+        int idade = 29;
+        float salario = 3500f;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+        System.out.println();
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlayStation = 5000f;
+        boolean isPlayStationCompravel = valorTotalContaCorrente > valorPlayStation || valorTotalContaPoupanca > valorPlayStation;
+        System.out.println("isPlayStationCompravel " + isPlayStationCompravel);
+        System.out.println();
+
+        // = += -= *= /= %=
+        double bonus = 1800; //1800
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        //== --
+        int contador =1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
+
     }
 }
