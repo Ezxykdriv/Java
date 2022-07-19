c5511754217603b8e73e51c2055526ea2614f1c0
Ezxykdriv <mm.matheusfalcao@gmai.com>
2022-07-18 22:58:35 -0400

    Operadores

---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Date;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+
+        String nome = "Matheus Falcão";
+        String endereco = "Brigadeiro Tobias, 542";
+        double salario = 6000.00;
+        String dataRecebimentoSalario ="18/07/22";
+
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + " confirmo que recebi o salário de " + salario + " reais, na data " + dataRecebimentoSalario );
+    }
+}
---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero02 = 20;
+        int numero01 = 10;
+        int resultado = numero02+numero01;
+        System.out.println(resultado);
+    }
+}
