1be560c550acd15cf579a42d8867f8991deb2773
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-07-20 13:33:18 -0400

    Aula01 Consumindo a API do IMDB

--- Alura/Imersão Java/alura-stickers/src/App.java
+++ Alura/Imersão Java/alura-stickers/src/App.java
@@ -1,11 +1,33 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         // fazer uma conxão HTTP e buscar os top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_jl200gd6";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        //System.out.println(body);
 
         // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println("Título: " + filme.get("title"));
+            System.out.println("Imagem: " + filme.get("image"));
+            System.out.println("Classificação: " + filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
