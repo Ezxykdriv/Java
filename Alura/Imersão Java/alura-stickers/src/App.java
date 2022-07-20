3acbbb35e6e62b70ca632f601674f789a65f1ba9
Ezxykdriv <mm.matheusfalcao@gmai.com>
2022-07-19 22:53:30 -0400

    Aula 01

---
+++ Alura/Imersão Java/alura-stickers/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // fazer uma conxão HTTP e buscar os top 250 filmes
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+
+        // exibir e manipular os dados
+    }
+}
