e5b5aa4dcdaf98681d23c309d40761deb030bbf2
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-07-27 18:10:49 -0400

    Arrays

---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float e double iniciam com 0
+        //char inicia com '\u0000' ' '
+        //boolean inicia com false
+        //String inicia com null
+
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+        nomes[3] = "Hinata";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
