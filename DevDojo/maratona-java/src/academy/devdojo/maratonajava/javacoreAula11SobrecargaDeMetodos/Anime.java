a89e548978af5f342cbe73f4c0c307d9d081b1b8
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-08-02 17:14:28 -0400

    Orientação Objetos : Sobrecarga de Métodos

---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacoreAula11SobrecargaDeMetodos/Anime.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacoreAula11SobrecargaDeMetodos;
+
+public class Anime {
+    private String tipo;
+    private int episodios;
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+}
