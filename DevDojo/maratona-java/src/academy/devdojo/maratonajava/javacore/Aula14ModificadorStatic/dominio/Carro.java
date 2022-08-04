89c8bb5cb01db528708c9b944cb84909d74871c3
Ezxykdriv <mm.matheusfalcao@gmai.com>
2022-08-03 20:24:12 -0400

    Orientação Objetos: Associação Arrays com Objetos

--- DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Aula14ModificadorStatic/dominio/Carro.java
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Aula14ModificadorStatic/dominio/Carro.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Aula14ModificadorStatic.dominio;
 public class Carro {
     private String nome;
     private double velocidadeMaxima;
-    private static double velocidadeLimite = 250;
+    public static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima) {
         this.nome = nome;
