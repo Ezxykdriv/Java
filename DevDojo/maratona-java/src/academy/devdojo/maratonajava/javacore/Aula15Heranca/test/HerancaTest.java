2b1fdb8e56bc93133c6d919e864a984fb8af828e
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-08-09 17:45:49 -0400

    Orientação Objetos : Heranca

---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Aula15Heranca/test/HerancaTest.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Aula15Heranca.test;
+
+import academy.devdojo.maratonajava.javacore.Aula15Heranca.dominio.*;
+
+public class HerancaTest {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shimazu");
+        pessoa.setCpf("1111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("22222");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        System.out.println("----------------");
+        funcionario.imprime();
+    }
+}
