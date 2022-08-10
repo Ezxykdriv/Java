f8258b21cd26b269ca4b69e6bf84ac0f6cdcf362
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-08-10 17:28:20 -0400

    Orientação Objetos : Heranca

---
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Aula17Sobrescrita/test/AnimeTest.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Aula17Sobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Aula17Sobrescrita.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr.Stone");
+
+        System.out.println(anime);
+    }
+}
