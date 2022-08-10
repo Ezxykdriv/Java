03d79a86f1ad794117635f80b327d82fe18435c2
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-08-10 17:14:37 -0400

    Orientação Objetos : Heranca

--- DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Aula15Heranca/test/HerancaTest02.java
+++ DevDojo/maratona-java/src/academy/devdojo/maratonajava/javacore/Aula15Heranca/test/HerancaTest02.java
@@ -6,7 +6,7 @@ public class HerancaTest02 {
     // 0 - Bloco de inicialização estático da super classe é executado quando a JVM carregar classe pai
     // 1 - Bloco de inicialização estático da subclasse é executado quando a JVM carregar classe filha
     // 2 - Alocado espaco em memória pro objeto da superclasse
-    // 3 - Cada atributo de superclasse é criado e inicializado com valores default ou o quer for passado
+    // 3 - Cada atributo de superclasse é criado e inicializado com valores default ou o que for passado
     // 4 - Bloco de inicialização da superclasse é executado na ordem em que aparece
     // 5 - Construtor é executado da superclasse
     // 6 - Alocado espaco em memória pro objeto da subclasse
