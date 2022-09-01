031d15dc031332b3710094a531d0f3d13af6d0a5
Ezxykdriv <mm.matheusfalcao@gmail.com>
2022-09-01 18:03:33 -0400

    Separando ações

--- Alura/Fromação Spring Framework/1 - Fundamentos do Java na Web/2 - Java Servlet autenticação, autorização e o padrão MVC/gerenciador/src/br/com/alura/gerenciador/servlet/RemoveEmpresaServlet.java
+++ Alura/Fromação Spring Framework/1 - Fundamentos do Java na Web/2 - Java Servlet autenticação, autorização e o padrão MVC/gerenciador/src/br/com/alura/gerenciador/servlet/RemoveEmpresaServlet.java
@@ -10,12 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 
-@WebServlet("/removeEmpresa")
+//@WebServlet("/removeEmpresa")
 public class RemoveEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void executa(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
--- Alura/Fromação Spring Framework/1 - Fundamentos do Java na Web/2 - Java Servlet autenticação, autorização e o padrão MVC/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ Alura/Fromação Spring Framework/1 - Fundamentos do Java na Web/2 - Java Servlet autenticação, autorização e o padrão MVC/gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,7 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.acao.AlteraEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
+import br.com.alura.gerenciador.acao.MostraEmpresa;
+import br.com.alura.gerenciador.acao.NovaEmpresa;
+import br.com.alura.gerenciador.acao.RemoveEmpresa;
 
 @WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
@@ -18,8 +22,7 @@ public class UnicaEntradaServlet extends HttpServlet {
 
 	}
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String paramAcao = request.getParameter("acao");
 
@@ -28,10 +31,26 @@ public class UnicaEntradaServlet extends HttpServlet {
 			ListaEmpresas acao = new ListaEmpresas();
 			acao.executa(request, response);
 
-		} else if (paramAcao.equals("RemovaEmpresa")) {
-			System.out.println("Removendo Empresa");
+		} else if (paramAcao.equals("RemoveEmpresa")) {
+			
+			RemoveEmpresa acao = new RemoveEmpresa();
+			acao.executa(request, response);
+			
 		} else if (paramAcao.equals("MostraEmpresa")) {
-			System.out.println("Mostrando dados da Empresa");
+			
+			MostraEmpresa acao = new MostraEmpresa();
+			acao.executa(request, response);
+			
+		} else if (paramAcao.equals("AlteraEmpresa")) {
+			
+			AlteraEmpresa acao = new AlteraEmpresa();
+			acao.executa(request, response);
+			
+		} else if (paramAcao.equals("NovaEmpresa")) {
+			
+			NovaEmpresa acao = new NovaEmpresa();
+			acao.executa(request, response);
+			
 		}
 	}
 
