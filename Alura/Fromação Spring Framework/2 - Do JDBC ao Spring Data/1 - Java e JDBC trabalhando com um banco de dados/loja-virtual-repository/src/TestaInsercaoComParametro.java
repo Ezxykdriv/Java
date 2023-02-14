2de9aa7949cfd26c27f7115d39ecb1f9dd1675f0
Ezxykdriv <mm.matheusfalcao@gmail.com>
2023-02-14 19:54:09 -0400

    Controle de transação

--- Alura/Fromação Spring Framework/2 - Do JDBC ao Spring Data/1 - Java e JDBC trabalhando com um banco de dados/loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ Alura/Fromação Spring Framework/2 - Do JDBC ao Spring Data/1 - Java e JDBC trabalhando com um banco de dados/loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -7,22 +7,39 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio); delete from Produto;";
-		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
+		try (Connection connection = connectionFactory.recuperarConexao()) {
 
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+			connection.setAutoCommit(false);
+
+			try (PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+				adicionarVariavel("SmarTV", "45 polegadas", stm);
+				adicionarVariavel("Radio", "Radio de bateria", stm);
+
+				connection.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+
+		if (nome.equals("Radio")) {
+			throw new RuntimeException("Não foi possível adicionar o produto");
+		}
 		stm.execute();
 
-		ResultSet rst = stm.getGeneratedKeys();
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O ID criado foi: " + id);
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O ID criado foi: " + id);
+			}
 		}
-
 	}
 }
